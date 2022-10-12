68263fc66bfe450daa4b515013005001645e4927
Hemish Ahuja <hemish.ahuja1@gmail.com>
2022-10-12 13:26:57 +1100

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' bank dataset
+#'
+#' A subset of data from the bank
+#' Report ...
+#'
+#' @format ## `bank`
+#' A data frame with 100 rows and 3 columns:
+#' \describe{
+#'   \item{...1}{customer number}
+#'   \item{arrival_time}{customer arrival time}
+#'   \item{service_time}{customer service time}
+#'   ...
+#' }
+#' @source STAT1378
+"bank"
---
+++ tests/testthat/test-Multiserver.R
@@ -0,0 +1,7 @@
+test_that("test Multiserver works", {
+  expect_identical(Multiserver(8,9),
+                   data.frame(Arrivals = 8, ServiceBegins = 8, ChosenServer = 1, ServiceEnds = 17))
+  expect_identical(Multiserver(45.32,28.19),
+                   data.frame(Arrivals = 45.32, ServiceBegins = 45.32, ChosenServer = 1, ServiceEnds = 73.51))
+  expect_error(Multiserver(-4,6))
+})
